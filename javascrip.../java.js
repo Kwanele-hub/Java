36eca3fb266a7a27ea04f8eceb30c6d383ffde9a
nolwazi749 <nolwazizwane7@gmail.com>
2020-10-14 02:58:17 -0400

    renamed my repo file

---
+++ javascrip.../java.js
@@ -0,0 +1,13 @@
+var nums = [2, 3, 5],
+    sum = 2*3*5,
+    
+
+
+for (var i = 0; i < nums.length; i++) {
+    if (i < nums.length - 1) {
+        
+        sum += nums[i];
+    } else {
+        
+        product = sum * nums[i];  
+    }
